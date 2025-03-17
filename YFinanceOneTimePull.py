792d7c094226615298361b83bb5adf72cbbfb5fb
Sam-I-Woodruff <122302128+Sam-I-Woodruff@users.noreply.github.com>
2025-03-17 14:27:45 -0600

    Got The Y Finance One Time Pull Working

---
+++ YFinanceOneTimePull.py
@@ -0,0 +1,30 @@
+import yfinance as yf
+import pandas as pd
+import time
+import os
+
+folder_path = 'C:\\Users\\samwo\\Documents\\Code\\Algo_Trading\\YFinance\\'
+tickers_df = pd.read_csv('C:\\Users\\samwo\\Documents\\Code\\Algo_Trading\\Tickers\\50Tickers.csv')
+tickers = tickers_df['Tickers'].tolist()
+
+# Ensure the folder exists
+os.makedirs(folder_path, exist_ok=True)
+
+for ticker in tickers:
+
+    print(f"Fetching data for {ticker}")
+
+    historical_data = yf.download(ticker, period="3y")
+
+    if not historical_data.empty:
+        file_path = os.path.join(folder_path, f"{ticker}.csv")
+        historical_data.to_csv(file_path)
+        print(f"Saved {ticker} data to {file_path}")
+    else:
+        print(f"No data found for {ticker}")
+
+    # print(historical_data[['Open', 'High', 'Low', 'Close', 'Volume']])
+    print(historical_data[['Close']])
+
+    # time.sleep(1)
+
